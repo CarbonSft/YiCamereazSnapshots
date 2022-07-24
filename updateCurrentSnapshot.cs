2537319e3d02ccba777d7f0898bdb4947d2f4923
Bartosz Węgielewski <bwegiel@gmail.com>
2022-07-24 12:24:46 +0200

    Initial commit.

---
+++ updateCurrentSnapshot.cs
@@ -0,0 +1,42 @@
+using System;
+using System.IO;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
+using System.Threading.Tasks;
+
+namespace CarbonSoftware.YiCamerazSnapshots
+{
+
+    public class updateCurrentSnapshot
+    {
+        const string ContanerName = "periodic-snapshots";
+        const string CurrentJpgName = "current.jpg";
+            
+        [FunctionName("updateCurrentSnapshot")]
+        public static async Task Run([BlobTrigger("periodic-snapshots/{name}", Connection = "yisnapshots_STORAGE")]Stream myBlob, string name, ILogger log)
+        {
+        
+            if(CurrentJpgName.Equals(name))return;
+
+            // retrieve the SOURCE and DESTINATION Storage Account Connection Strings
+            var sourceConnString = Environment.GetEnvironmentVariable("yisnapshots_STORAGE");
+            var destConnString = Environment.GetEnvironmentVariable("yisnapshots_STORAGE");
+
+            // Create SOURCE Blob Client
+            var sourceBlobClient = new BlobClient(sourceConnString, "periodic-snapshots", name);
+
+            // Generate SAS Token for reading the SOURCE Blob with a 2 hour expiration
+            var sourceBlobSasToken = sourceBlobClient.GenerateSasUri(BlobSasPermissions.Read, DateTimeOffset.Now.AddHours(2));
+
+            // Create DESTINATION Blob Client
+            var destBlobClient = new BlobClient(destConnString, "periodic-snapshots", CurrentJpgName);
+
+            // Initiate Blob Copy from SOURCE to DESTINATION
+            await destBlobClient.StartCopyFromUriAsync(sourceBlobSasToken);
+
+            log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
+        }
+    }
+}
